0c901ab8076bbb7358f08b1f9e2fb206193ceb32
ecin <ecin@copypastel.com>
2014-09-05 01:34:15 -0700

    Add class description and examples.

--- lib/redis/mutex.rb
+++ lib/redis/mutex.rb
@@ -1,6 +1,34 @@
 require "redis"
 require "securerandom"
 
+# Public: A Redis-backed mutex implementation that is compatible with Ruby's
+# standard library Mutex class. Redis::Mutex can be used as a distributed lock;
+# multiple processes on different machines can use Redis::Mutex to coordinate their
+# actions.
+#
+# Examples
+#
+#   redis = Redis.new
+#
+#   # Redis::Mutex.new creates a mutex with a random key; that is, the key that's inserted
+#   # into the Redis server to create the lock will be random.
+#   mutex = Redis::Mutex.new(redis)
+#   mutex.lock
+#   redis.get(mutex.key) # returns a uuid identifying the lock
+#
+#   # To coordinate across different processes, a predetermined key name is preferable.
+#   Redis::Mutex.new(redis, key: "scheduler_lock")
+#
+#   # Finally, a timeout determines how long the lock is valid for. This guarantees that
+#   # the lock will eventually be available, even if the original process that created the
+#   # lock disappears.
+#   Redis::Mutex.new(redis, key: "scheduler_lock", timeout: 120)
+#
+#   # Once locked, a Redis::Mutex will be locked until the timeout transpires. The lock can
+#   # be refreshed to extend the expiration time of the lock.
+#   mutex = Redis::Mutex.new(redis)
+#   mutex.lock
+#   mutex.refresh # will reset the lock's ttl to mutex.timeout's value
 class Redis::Mutex
 
   UNLOCK_SCRIPT = <<-SCRIPT.freeze
